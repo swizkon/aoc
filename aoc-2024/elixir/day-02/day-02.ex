e315faad059ce0f5040ba7ef56c082614fb68bb6
swizkon <swizkon@gmail.com>
2024-12-03 01:31:35 +0100

    hacky brute for day 02 problem 2

--- aoc-2024/elixir/day-02/day-02.ex
+++ aoc-2024/elixir/day-02/day-02.ex
@@ -1,4 +1,4 @@
-defmodule User do
+defmodule ReportValidation do
   defstruct prev: -1, status: 0, incrs: 0, decrs: 0, dups: 0, errors: 0
 end
 
@@ -11,9 +11,13 @@ defmodule Day02 do
 
     IO.inspect(reports, label: "reports")
 
-    safe_ones = reports
+    safe_reports = reports
       |> map(&(validate_report(&1)))
-      |> Enum.filter(fn x -> x.status == 0 end)
+      |> Enum.flat_map(fn x -> x end)
+
+    IO.inspect(safe_reports, label: "safe_reports")
+    safe_reports
+      # |> Enum.filter(fn x -> x.status == 0 end)
       |> length
   end
 
@@ -24,27 +28,80 @@ defmodule Day02 do
   end
 
   defp validate_report(report) do
+
+    # res = validate_single_report(report)
+
+    # IO.inspect(res, label: "res")
+
+    # if res.status == 0 do
+    #   IO.inspect("no errors", label: "no errors")
+    #   [res]
+    # end
+
+    IO.inspect("loose_validate_report", label: "loose_validate_report")
+    loose_validate_report(report)
+      |> Enum.filter(fn x -> x.status == 0 end)
+      |> Enum.take(1)
+
+    # Enum.reduce_while(report, %ReportValidation{}, fn x, acc ->
+    #   cond do
+    #     acc.prev < 0 ->
+    #       {:cont, %{acc | prev: x}}
+
+    #     acc.prev == x ->
+    #       {:halt, %{acc | status: 1, dups: acc.dups + 1}}
+
+    #     abs(x - acc.prev) > 3 ->
+    #       {:halt, %{acc | status: 4}}
+
+    #     x < acc.prev and acc.incrs > 0 ->
+    #       {:halt, %{acc | status: 2}}
+
+    #     x < acc.prev ->
+    #       {:cont, %{acc | prev: x, decrs: acc.decrs + 1}}
+
+    #     x > acc.prev and acc.decrs > 0  ->
+    #       {:halt, %{acc | status: 3}}
+
+    #     true ->
+    #       {:cont, %{acc | prev: x, incrs: acc.incrs + 1}}
+    #   end
+    # end)
+
+  end
+
+  defp loose_validate_report(report) do
     IO.inspect(report, label: "report")
 
-    Enum.reduce_while(report, %User{}, fn x, acc ->
+    loose_reports = transform_to_loose(report)
+
+    IO.inspect(loose_reports, label: "loose_reports")
+
+    Enum.map(loose_reports, fn x -> validate_single_report(x) end)
+
+
+  end
+
+  defp validate_single_report(report) do
+    Enum.reduce_while(report, %ReportValidation{}, fn x, acc ->
       cond do
         acc.prev < 0 ->
           {:cont, %{acc | prev: x}}
 
         acc.prev == x ->
-          {:halt, %{acc | status: 1, dups: acc.dups + 1}}
+          {:cont, %{acc | prev: x, status: 1, dups: acc.dups + 1, errors: acc.errors + 1}}
 
         abs(x - acc.prev) > 3 ->
-          {:halt, %{acc | status: 4}}
+          {:cont, %{acc | status: 4, errors: acc.errors + 1}}
 
         x < acc.prev and acc.incrs > 0 ->
-          {:halt, %{acc | status: 2}}
+          {:cont, %{acc | status: 2, errors: acc.errors + 1}}
 
         x < acc.prev ->
-          {:cont, %{acc | prev: x, decrements: acc.decrements + 1}}
+          {:cont, %{acc | prev: x, decrs: acc.decrs + 1}}
 
-        x > acc.prev and acc.decrements > 0  ->
-          {:halt, %{acc | status: 3}}
+        x > acc.prev and acc.decrs > 0  ->
+          {:cont, %{acc | status: 3, errors: acc.errors + 1}}
 
         true ->
           {:cont, %{acc | prev: x, incrs: acc.incrs + 1}}
@@ -53,11 +110,17 @@ defmodule Day02 do
 
   end
 
+  defp transform_to_loose(list) do
+    Enum.map(Enum.with_index(list), fn {_, index} ->
+      List.delete_at(list, index)
+    end)
+  end
+
 
 end
 
 
-data = File.read!("day-02-test.txt")
+data = File.read!("day-02.txt")
 
 safe_reports = Day02.safe_reports(data)
 IO.inspect(safe_reports, label: "safe_reports")
