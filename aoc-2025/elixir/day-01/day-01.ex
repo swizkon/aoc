d1daa4ef4f39bd44525c2651da9b038dbe3a2f74
swizkon <swizkon@gmail.com>
2025-12-04 23:42:07 +0100

    jhgjgh

--- aoc-2025/elixir/day-01/day-01.ex
+++ aoc-2025/elixir/day-01/day-01.ex
@@ -8,44 +8,55 @@ defmodule Day01 do
       |> replace("R", "+")
       |> split("\r\n", trim: true)
       |> map(&to_integer/1)
-      |> Enum.reduce([50], fn x, acc -> accumulator(x, acc) end)
+      |> Enum.reduce([50], fn x, acc -> pos_accumulator(x, acc) end)
 
       IO.inspect(data |> Enum.reverse, label: "Positions")
      data |> Enum.count(&(&1 == 0))
   end
 
-  defp accumulator(rotation, state) do
+  defp pos_accumulator(rotation, state) do
     curr_pos = hd(state)
     new_pos = rem(curr_pos + rem(rotation, 100), 100)
-
-    # To get the count of zero passes, check if old and new positions cross zero
-    # This logic can be adjusted based on specific requirements
-    # if (curr_pos > 0 and new_pos <= 0) or (curr_pos < 0 and new_pos >= 0) do
-
     [new_pos | state]
   end
 
   def get_zero_passes(input) do
-    initial_state = [50]
+    initial_state = {50, 0} # Starting at position 50 with 0 zero passes
     input
       |> replace("L", "-")
       |> replace("R", "+")
       |> split("\r\n", trim: true)
       |> map(&to_integer/1)
       |> Enum.reduce(initial_state, fn x, acc -> acc_zero_passes(x, acc) end)
-      |> Enum.count(&(&1 == 0))
   end
 
   defp acc_zero_passes(rotation, state) do
-    curr_pos = hd(state)
+    {curr_pos, total_passes} = state
     new_pos = rem(curr_pos + rem(rotation, 100), 100)
 
-    # To get the count of zero passes, check if old and new positions cross zero
-    # This logic can be adjusted based on specific requirements
-    # if (curr_pos > 0 and new_pos <= 0) or (curr_pos < 0 and new_pos >= 0) do
+    # How many full passes through zero
+    full_passes = div(abs(rotation), 100)
 
-    [new_pos | state]
+    zero_passes =
+      cond do
+        new_pos > curr_pos and rotation < 0 -> 1
+        new_pos < curr_pos and rotation > 0 -> 1
+
+        curr_pos < new_pos and curr_pos < 0 and new_pos > 0 -> 1
+        curr_pos > new_pos and curr_pos > 0 and new_pos < 0 -> 1
+
+        new_pos == 0 -> 1
+        true -> 0
+      end + full_passes
+
+    IO.puts ("Curr Pos: #{curr_pos}, Rotation: #{rotation}, New Pos: #{new_pos}, Zero Passes: #{zero_passes}")
+
+    fixed_p = fix_position(new_pos)
+    {fixed_p, total_passes + zero_passes}
   end
 
+  defp fix_position(position) when position < 0, do: position + 100
+  defp fix_position(position) when position > 99, do: position - 100
+  defp fix_position(position), do: position
 
 end
--- aoc-2025/elixir/solve_some.exs
+++ aoc-2025/elixir/solve_some.exs
@@ -1,11 +1,12 @@
 Code.require_file("day-01/day-01.ex", __DIR__)
 
 
-# data = File.read!("../inputs/day-01.txt")
-data = File.read!("../inputs/day-01-test.txt")
+data = File.read!("../inputs/day-01.txt")
+# data = File.read!("../inputs/day-01-test.txt")
+# data = File.read!("../inputs/day-01-test-ext.txt")
 
-result = Day01.get_zero_positions(data)
-IO.inspect(result, label: "get_zero_positions")
+# result = Day01.get_zero_positions(data)
+# IO.inspect(result, label: "get_zero_positions")
 
 result = Day01.get_zero_passes(data)
 IO.inspect(result, label: "get_zero_passes")
