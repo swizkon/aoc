d78fe93b53919dc9637f794b90508065a934b4ae
swizkon <swizkon@gmail.com>
2025-12-05 00:55:12 +0100

    yeah yeah

--- aoc-2025/elixir/day-02.ex
+++ aoc-2025/elixir/day-02.ex
@@ -1,6 +1,5 @@
 defmodule Day02 do
-  # import Enum, only: [map: 2]
-  import String, only: [replace: 3, split: 3, to_integer: 1, split_at: 2]
+  import String, only: [split: 3, to_integer: 1, split_at: 2]
 
   import Stream, only: [map: 2]
 
@@ -9,14 +8,13 @@ defmodule Day02 do
       |> split(",", trim: true)
       |> map(&to_int_range/1)
       |> Enum.flat_map(& &1)
-      |> Enum.filter(&(is_invalid_id(&1)))
-      # |> Enum.uniq()
+      |> Enum.filter(&(is_any_invalido_id(&1)))
       |> Enum.sum()
   end
 
   defp to_int_range(range) do
     [start_s, end_s] = String.split(range, "-")
-    result = Enum.to_list(to_integer(start_s)..to_integer(end_s))
+    Enum.to_list(to_integer(start_s)..to_integer(end_s))
   end
 
   defp is_invalid_id(id) do
@@ -25,6 +23,29 @@ defmodule Day02 do
     half_length = div(String.length(s_id), 2)
     {left_half, right_half} = split_at(s_id, half_length)
     left_half == right_half
+  end
+
+
+  defp is_any_invalido_id(id) do
+    s_id = to_string(id)
+
+    half_length = div(String.length(s_id), 2)
+
+    1..half_length
+      |> Enum.map(&(is_any_invalid_id(&1, s_id)))
+      |> Enum.any?()
+
+    # IO.puts("ID: #{s_id} - Results: #{inspect(data)}")
+# data
+  end
+
+  defp is_any_invalid_id(chunk_size, s_id) do
+    s_id
+      |> String.graphemes()
+      |> Enum.chunk_every(chunk_size)
+      |> Enum.map(&Enum.join/1)
+      |> Enum.uniq()
+      |> Enum.count() == 1
   end
 
 end
--- aoc-2025/elixir/solve_some.exs
+++ aoc-2025/elixir/solve_some.exs
@@ -10,17 +10,15 @@
 # result = Day01.get_zero_passes(data)
 # IO.inspect(result, label: "get_zero_passes")
 
-
-
 #################################################################
 # DAY 02
 
 Code.require_file("day-02.ex", __DIR__)
 
-data = File.read!("../inputs/day-02-test.txt")
+# data = File.read!("../inputs/day-02-test.txt")
 data = File.read!("../inputs/day-02.txt")
 result = Day02.sum_invalid_ids(data)
 
-correct = result == 1_227_775_554
+correct = result == 4_174_379_265
 IO.inspect(result, label: "sum_invalid_ids")
 IO.inspect(correct, label: "Correct?")
