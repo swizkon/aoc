00fe97b343e25f8a6ee7fff1e201f80820874174
swizkon <swizkon@gmail.com>
2025-12-10 07:33:42 +0100

    add d3

--- aoc-2025/elixir/day-03.ex
+++ aoc-2025/elixir/day-03.ex
@@ -5,28 +5,72 @@ defmodule Day03 do
   def sum_joltage(input) do
     input
       |> S.split("\r\n", trim: true)
-      |> E.map(&S.graphemes/1)
-      |> E.map(&to_ints/1)
-      |> E.map(&find_max_pair/1)
+      |> E.map(&transform_data/1)
       |> E.sum()
   end
 
-  defp to_ints(list) do
-    Enum.map(list, &String.to_integer/1)
+  def sum_joltage_bigpack(input, pack_size) do
+
+    IO.puts "input: #{inspect input} pack_size: #{inspect pack_size}"
+    data = input
+      |> S.split("\r\n", trim: true)
+      |> E.map(&transform_data/1)
+
+    # IO.puts "data: #{inspect data}"
+
+    initial_state = []
+
+    data
+    #  |> E.sum()
   end
 
-  defp find_max_pair(data) do
-    indexed = data |> Enum.with_index()
-    first = indexed
-      |> Enum.drop(-1)
-      |> Enum.max_by(fn {value, _idx} -> value end)
+  defp transform_data(input) do
+    data = input
+      |> S.graphemes
+      |> E.map(&S.to_integer/1)
+      # |> Enum.reverse
+      |> Enum.with_index
+      # |> Enum.reverse
 
-    IO.puts "first: #{inspect first}"
-
-    second = indexed
-      |> Enum.drop(elem(first, 1) + 1)
-      |> Enum.max_by(fn {value, _idx} -> value end)
-    S.to_integer("#{elem(first, 0)}#{elem(second, 0)}")
+    IO.puts("data: #{inspect data}")
+    data
+      |> find_max_pair(3, "")
   end
+
+  defp find_max_pair([], _, acc) do
+
+    IO.puts("find_max_pair No more to calc...")
+    acc
+  end
+
+  defp find_max_pair(index_list, pack_size, acc) do
+    highest_ranked_within_array = index_list
+      |> Enum.slice(0, pack_size)
+      |> Enum.max_by(fn {value, _idx} -> value end)
+
+    IO.puts "highest_ranked_within_array: #{inspect highest_ranked_within_array}"
+
+    remain = index_list
+      |> Enum.drop(elem(highest_ranked_within_array, 1) + 1)
+
+    acc = acc <> "#{elem(highest_ranked_within_array, 0)}"
+
+    IO.puts "remain: #{inspect remain} pack_size: #{inspect pack_size} acc: #{inspect acc}"
+    find_max_pair(remain, pack_size, acc)
+  end
+
+  # defp find_max_pair(data) do
+  #   indexed = data
+  #   first = indexed
+  #     |> Enum.drop(-1)
+  #     |> Enum.max_by(fn {value, _idx} -> value end)
+
+  #   IO.puts "first: #{inspect first}"
+
+  #   second = indexed
+  #     |> Enum.drop(elem(first, 1) + 1)
+  #     |> Enum.max_by(fn {value, _idx} -> value end)
+  #   S.to_integer("#{elem(first, 0)}#{elem(second, 0)}")
+  # end
 
 end
